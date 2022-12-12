0533ca8e358d1be0c4a1e04c8243d631998ce8a1
juliavel <juli.avelchuk@gmail.com>
2022-12-12 20:17:30 +0300

    admin site

--- university/admin.py
+++ university/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
 
-from university.models import Group, Student
+from university.models import Group, Student, Lesson
 
 
-admin.site.register(Group)
-admin.site.register(Student)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name', )
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'group')
+    search_fields = ('last_name', 'first_name', 'group')
+    list_filter = ('group', )
+    list_select_related = ('group', )
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('time', 'room', 'group')
+    list_filter = ('time', 'room', 'group')
+    list_select_related = ('group',)
+    actions = ['cancel_class']
+
+    @admin.action(description='Отменить занятие')
+    def cancel_class(self, request, queryset):
+        queryset.delete()
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Lesson, LessonAdmin)
--- university/models.py
+++ university/models.py
@@ -36,3 +36,28 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.last_name} {self.first_name}'
+
+
+class Lesson(models.Model):
+    class Rooms(models.TextChoices):
+        FIRST = '001', '001'
+        SECOND = '002', '002'
+        THIRD = '003', '003'
+
+    class Time(models.TextChoices):
+        NINE = '9', '09:00'
+        ELEVEN = '11', '11:00'
+        THIRTEEN = '13', '13:00'
+
+    group = models.ForeignKey(
+        Group, verbose_name='Группа', on_delete=models.CASCADE,
+    )
+    room = models.CharField('Аудитория', choices=Rooms.choices, max_length=50)
+    time = models.CharField('Время начала', choices=Time.choices, max_length=50)
+
+    class Meta:
+        verbose_name = 'Занятие'
+        verbose_name_plural = 'Занятия'
+
+    def __str__(self):
+        return f'{self.time}: {self.group} {self.room}'
